f3b559925f39e820ae1dfd88613161ba9e2aae97
James Thompson <4791949+jamesthomp@users.noreply.github.com>
2024-05-09 15:39:09 +0100

    remove non svg

--- tests/test.js
+++ tests/test.js
@@ -1,19 +1,16 @@
 #!/usr/bin/env node
 
-var fs = require('fs');
+var fs = require("fs");
 function file(name) {
-    return fs.createWriteStream(__dirname + '/' + name);
+  return fs.createWriteStream(__dirname + "/" + name);
 }
 
-var qr = require('./../');
-var text = 'I \u2764\uFE0F QR code!';
-var text = 'https://yadi.sk/d/FuzPeEg-QyaZN?qr';
-var ec_level = 'Q';
+var qr = require("./../");
+var text = "I \u2764\uFE0F QR code!";
+var text = "https://yadi.sk/d/FuzPeEg-QyaZN?qr";
+var ec_level = "Q";
 
-qr.image(text, { type: 'png', ec_level: ec_level, parse_url: false, margin: 1}).pipe(file('qr_f.png'));
-qr.image(text, { type: 'png', ec_level: ec_level, parse_url: true,  margin: 1}).pipe(file('qr_t.png'));
-qr.image(text, { type: 'svg', ec_level: ec_level}).pipe(file('qr.svg'));
-qr.image(text, { type: 'eps', ec_level: ec_level}).pipe(file('qr.eps'));
-qr.image(text, { type: 'pdf', ec_level: ec_level}).pipe(file('qr.pdf'));
-
-fs.writeFileSync('qr_sync.png', qr.imageSync(text));
+fs.writeFileSync(
+  "qr_sync.svg",
+  qr.imageSync(text, { type: "svg", ec_level: ec_level })
+);
